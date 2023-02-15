a4035b040122db045660c2371a47371c92b66843
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2023-02-15 17:01:09 -0300

    chore: install styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
+import GlobalStyle from "./styles/globalStyles";
 
 export default function App() {
   return (
+    <>
+    <GlobalStyle />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Signup />}/>
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
