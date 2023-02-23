c23d5ee67a66704b09e8eca02094f736ea1ae8df
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2023-02-23 18:09:24 -0300

    feat; create render page to secrets

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,19 +1,42 @@
 import styled from "styled-components";
 import Title from "../components/Title";
+import SecretsType from "../renderpages/SecretsType";
 
 export default function Main() { 
+    const typeSecrets = [ 
+        {
+            id: 1, 
+            name: "Credentials",
+            icon: "enter"
+        },
+        {
+            id: 2, 
+            name: "Safe Notes",
+            icon: "pencil-sharp"
+        },
+        {
+            id: 3, 
+            name: "Cards",
+            icon: "card"
+        },
+        {
+            id: 4, 
+            name: "Wifi Passwords",
+            icon: "wifi"
+        },
+    ]
     return(
         <>
         <Title />
         <Types> 
             <ul>
-                <Type>
-                    <TypeInfo>
-                        <ion-icon name="wifi"></ion-icon>
-                        <span>Wifi passwords</span>
-                    </TypeInfo>
-                    <CounterBall>4</CounterBall>
-                </Type>
+                {typeSecrets.map(secret => (
+                    <SecretsType
+                        id= {secret.id}
+                        name= {secret.name}
+                        icon= { secret.icon}
+                    />
+                ))}
             </ul>
         </Types>
         </>
@@ -30,48 +53,4 @@ const Types = styled.div`
     ul { 
         width: 90%;
     }
-`
-const Type = styled.li`
-    width: 100%; 
-    height: 80px;
-    display: flex; 
-    align-items: center;
-    justify-content: space-between;
-    border-radius: 10px;
-    padding: 0px 10px;
-
-    &:hover,
-    &:focus { 
-        cursor: pointer;
-        background-color: white;
-    }
-`
-const TypeInfo = styled.div`
-    width: 50%; 
-    height: 100%; 
-    display: flex; 
-    align-items: center;
-
-    ion-icon { 
-        width: 50px; 
-        height: 50px;
-        color: rgba(0, 89, 133, 1);
-    }
-
-    span { 
-        font-size: 18px; 
-        font-weight: bold;
-        margin-left: 25px;
-    }
-`
-const CounterBall = styled.div`
-    width: 40px; 
-    height: 40px; 
-    border-radius: 50%; 
-    background-color: rgba(0, 89, 133, 1);
-    display: flex; 
-    justify-content: center; 
-    align-items: center;
-    color: white;
-    font-weight: bold;
 `
---
+++ src/renderpages/SecretsType.js
@@ -0,0 +1,60 @@
+import styled from "styled-components"
+
+export default function SecretsType({ id, name, icon }) { 
+    return(
+        <>
+        <Type id={id}>
+            <TypeInfo>
+                <ion-icon name={icon}></ion-icon>
+                <span>{name}</span>
+            </TypeInfo>
+            <CounterBall>4</CounterBall>
+        </Type>
+        </>
+    )
+}
+
+const Type = styled.li`
+    width: 100%; 
+    height: 80px;
+    display: flex; 
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 10px;
+    padding: 0px 10px;
+
+    &:hover,
+    &:focus { 
+        cursor: pointer;
+        background-color: white;
+    }
+`
+const TypeInfo = styled.div`
+    width: 50%; 
+    height: 100%; 
+    display: flex; 
+    align-items: center;
+
+    ion-icon { 
+        width: 50px; 
+        height: 50px;
+        color: rgba(0, 89, 133, 1);
+    }
+
+    span { 
+        font-size: 18px; 
+        font-weight: bold;
+        margin-left: 25px;
+    }
+`
+const CounterBall = styled.div`
+    width: 40px; 
+    height: 40px; 
+    border-radius: 50%; 
+    background-color: rgba(0, 89, 133, 1);
+    display: flex; 
+    justify-content: center; 
+    align-items: center;
+    color: white;
+    font-weight: bold;
+`
