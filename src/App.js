cea23c06578a62a566a7fc4cf90f675edeb0473f
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2023-02-24 16:45:19 -0300

    feat: config context variables

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Login from "./pages/Login";
 import Main from "./pages/Main";
 import Signup from "./pages/Signup";
 import GlobalStyle from "./styles/globalStyles";
+import TokenContext from "./contexts/tokenContext";
 
 export default function App() {
   const [ token, setToken ] = useState(localStorage.getItem("USER_TOKEN"));
-  
+
   return (
-    <>
-    <GlobalStyle />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/sign-up" element={<Signup />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/" element={<Main />}/>
-      </Routes>
-    </BrowserRouter>
-    </>
+    <TokenContext.Provider value={{ token, setToken }}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign-up" element={<Signup />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/" element={<Main />}/>
+        </Routes>
+      </BrowserRouter>
+    </TokenContext.Provider>
   );
 }
